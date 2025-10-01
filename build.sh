65f7caa92bb7d96403180ddc32bd65bd18eb28dc
Eberhard Beilharz <eb1@sil.org>
2025-10-01 15:49:24 +0200

    Add build script

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [[ "${#}" -gt 0 && "$1" != "test" ]]; then
+    echo "Only supported option is 'test'"
+    exit 0
+fi
+
+npm ci
+
+MOCHA_FLAGS=()
+
+if [[ -v TEAMCITY_GIT_PATH ]]; then
+  MOCHA_FLAGS+=(--reporter "$(dirname "$0")/tools/mocha-teamcity-reporter/teamcity.cjs" --reporter-options parentFlowId="unit_tests")
+  echo "##teamcity[flowStarted flowId='unit_tests']"
+fi
+
+npx mocha "${MOCHA_FLAGS[@]}" src/hello-world.tests.js
+
+if [[ -v TEAMCITY_GIT_PATH ]]; then
+  echo "##teamcity[flowFinished flowId='unit_tests']"
+fi
