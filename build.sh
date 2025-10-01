f5eb81194860d79d10dccae1ab8cdb9507cb314f
Eberhard Beilharz <eb1@sil.org>
2025-10-01 16:14:34 +0200

    Output TEAMCITY_GIT_PATH

--- build.sh
+++ build.sh
@@ -9,6 +9,8 @@ npm ci
 
 MOCHA_FLAGS=()
 
+echo "TEAMCITY_GIT_PATH=${TEAMCITY_GIT_PATH}"
+
 if [[ -v TEAMCITY_GIT_PATH ]]; then
   MOCHA_FLAGS+=(--reporter "$(dirname "$0")/tools/mocha-teamcity-reporter/teamcity.cjs")
   # MOCHA_FLAGS+=(--reporter "$(dirname "$0")/tools/mocha-teamcity-reporter/teamcity.cjs" --reporter-options parentFlowId="unit_tests")
