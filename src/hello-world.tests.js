bd93d83f5eded8dbacae243c475439f2b6ec0eeb
Eberhard Beilharz <eb1@sil.org>
2025-10-02 17:36:56 +0200

    Add failing and skipped tests

--- src/hello-world.tests.js
+++ src/hello-world.tests.js
@@ -7,5 +7,14 @@ describe('top level suite', function () {
     it("2 + 2", () => {
       assert.equal(2 + 2, 4);
     });
+    it.skip("20 + 2", () => {
+      assert.equal(20 + 2, 22);
+    });
+    it("4 + 2", () => {
+      assert.equal(4 + 2, 4);
+    });
+    it("3 + 3", () => {
+      assert.equal(3 + 3, 6);
+    });
   });
 });
